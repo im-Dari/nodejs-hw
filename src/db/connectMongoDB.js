50b97cf0acbc5fc7e2d7034142ffc587fa097629
Dasha <licht@MacBook-Air-Dasha.local>
2026-05-07 18:54:16 +0200

    fix controller functions

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
